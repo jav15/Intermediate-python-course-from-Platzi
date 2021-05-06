7d0294b4c4caec25fbbcfefe0ad35fb71dd2170b
Jav <iglesiasjavier15@gmail.com>
2021-05-06 12:36:15 -0500

    dubugging, lambda, comprehension

--- dict_comprenhensions.py
+++ dict_comprenhensions.py
@@ -5,7 +5,8 @@ def run():
     #         my_dict[i] = i**3
     # my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
     # print(my_dict)
-    my_dict = {i : round(i**0.5,2) for i in  range(1,1001)}
+    # Save in dictionary the first square root 100 numbers 
+    my_dict = {i : round(i**0.5,2) for i in  range(1,10)}
     print(my_dict)
 
 if __name__ == "__main__":
